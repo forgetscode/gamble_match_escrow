89fa974f59169bc1865d0571f0b3f769cae018a1
Charles Anderson <hpcngmoh@gmail.com>
2021-09-29 18:42:16 -0400

    init commit

---
+++ tests/gamble_match_escrow.js
@@ -0,0 +1,16 @@
+const anchor = require('@project-serum/anchor');
+
+
+
+describe('gamble_match_escrow', () => {
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  it('Is initialized!', async () => {
+    // Add your test here.
+    const program = anchor.workspace.GambleMatchEscrow;
+    const tx = await program.rpc.initialize();
+    console.log("Your transaction signature", tx);
+  });
+});
