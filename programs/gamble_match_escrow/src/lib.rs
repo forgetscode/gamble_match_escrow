014ab45e8b6ecfb2e2304780f544c438015ef762
MichaelGerg <michael@DESKTOP-G0G3AVA.localdomain>
2021-09-29 16:33:28 -0700

    test

--- programs/gamble_match_escrow/src/lib.rs
+++ programs/gamble_match_escrow/src/lib.rs
@@ -1,66 +1,32 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, SetAuthority, TokenAccount, Transfer};
-use spl_token::instruction::AuthorityType;
+
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod gamble_match_escrow {
     use super::*;
-    pub fn initialize(
-        ctx: Context<Initialize>,
-        // min_fee: u64,
-    ) -> ProgramResult {
-        ctx.accounts.match_account.initializer_key = *ctx.accounts.initializer.key;
-        ctx.accounts
-            .match_account
-            .initializer_deposit_token_account = *ctx
-            .accounts
-            .match_account
-            .to_account_info()
-            .key;
-        ctx.accounts
-            .match_account
-            .initializer_receive_token_account = *ctx
-            .accounts
-            .match_account
-            .to_account_info()
-            .key;
-        // let (pda, _bump_seed) = Pubkey::find_program_address(&[b"escrow"], ctx.program_id);
-        // token::set_authority(ctx.accounts.into(), AuthorityType::AccountOwner, Some(pda))?;
+    pub fn initialize_escrow(ctx: Context<InitializeEscrow>) -> ProgramResult {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(signer)]
-    pub initializer: AccountInfo<'info>,
+pub struct InitializeEscrow<'info> {
+    #[account(init, payer = user, space = 64)]
+    pub escrow_account: Account<'info, MatchAccount>,
     #[account(mut)]
-    pub match_account: Account<'info, MatchAccount>,
-    // pub match_account: Account<'info, EscrowAccount>,
-    // pub token: AccountInfo<'info>,
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
-pub type MatchDataArray<'info> = &'info [u16];
+/*
+pub struct UserItem {
+    pub account_key: Pubkey,
+    pub balance: u16
+}
+*/
+
 #[account]
 pub struct MatchAccount {
-    pub
-    pub initializer_deposit_token_account: Pubkey,
-    pub initializer_receive_token_account: Pubkey,
+    pub user_data:[u64; 5]
 }
-
-// impl<'info> From<&mut Initialize<'info>>
-//     for CpiContext<'_, '_, '_, 'info, SetAuthority<'info>>
-// {
-//     fn from(accounts: &mut Initialize<'info>) -> Self {
-//         let cpi_accounts = SetAuthority {
-//             account_or_mint: accounts
-//                 .initializer_fees_account
-//                 .to_account_info()
-//                 .clone(),
-//             current_authority: accounts.initializer.clone(),
-//         };
-//         let cpi_program = accounts.token_program.clone();
-//         CpiContext::new(cpi_program, cpi_accounts)
-//     }
-// }
--- tests/gamble_match_escrow.js
+++ tests/gamble_match_escrow.js
@@ -1,16 +1,36 @@
+const assert = require('assert');
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
 
 
+
+const provider = anchor.Provider.local()
+
+anchor.setProvider(provider)
+
+const program = anchor.workspace.GambleMatchEscrow;
+
+const testaccount = anchor.web3.Keypair.generate()
 
 describe('gamble_match_escrow', () => {
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  
 
   it('Is initialized!', async () => {
     // Add your test here.
-    const program = anchor.workspace.GambleMatchEscrow;
-    const tx = await program.rpc.initialize();
+    const tx = await program.rpc.initializeEscrow( {
+      accounts:{
+        escrow_account: testaccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [testaccount],
+    });
+
+    let escrow_account = await program.account.testaccount.fetch(testaccount.publicKey)
+
+    assert.ok(escrow_account);
     console.log("Your transaction signature", tx);
   });
 });
