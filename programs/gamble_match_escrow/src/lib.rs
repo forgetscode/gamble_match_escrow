a14ab21c57bd6570030c2847f9d0685e641099f6
MichaelGerg <michael@DESKTOP-G0G3AVA.localdomain>
2021-09-30 17:15:36 -0700

    vault

--- programs/gamble_match_escrow/src/lib.rs
+++ programs/gamble_match_escrow/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, SetAuthority, TokenAccount, Transfer};
+use anchor_spl::token::{self, SetAuthority, TokenAccount, Transfer, Mint};
 use spl_token::instruction::AuthorityType;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
@@ -8,23 +8,48 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod gamble_match_escrow {
     use super::*;
 
+    /*
     const ESCROW_PDA_SEED: &[u8] = b"escrow";
-
+    const _vault_account_bump: u8 = 4;
+    const initializer_amount: u8 = 4;
+    */
+    
     pub fn initialize_escrow(ctx: Context<InitializeEscrow>) -> ProgramResult {
         let match_account = &mut ctx.accounts.escrow_account;
-        match_account.user_data = [0,0,0,0,0];
+        match_account.user_data = [];
+
+        let (vault_authority, _vault_authority_bump) =
+            Pubkey::find_program_address(&[b"authority-seed"], ctx.program_id);
+        token::set_authority(
+            ctx.accounts.into_set_authority_context(),
+            AuthorityType::AccountOwner,
+            Some(vault_authority),
+        )?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct InitializeEscrow<'info> {
-    #[account(init, payer = user, space = 48)]
+    #[account(mut, signer)]
+    pub initializer: AccountInfo<'info>,
+    pub mint: Account<'info, Mint>,
+
+    #[account(mut)]
+    pub initializer_deposit_token_account:Account<'info, TokenAccount>,
+
+    #[account(init, payer = initializer, space = 100)]
     pub escrow_account: Account<'info, MatchAccount>,
-    #[account(mut)]
+
+    #[account(
+        init,
+        payer = initializer,
+        token::mint = mint,
+        token::authority = initializer
+    )]
     pub vault_account: Account<'info, TokenAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,
+    
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
     pub token_program: AccountInfo<'info>,
@@ -39,5 +64,16 @@ pub struct UserItem {
 
 #[account]
 pub struct MatchAccount {
-    pub user_data:[u64; 5]
+    pub user_data:[Pubkey; 0]
+}
+
+impl<'info> InitializeEscrow<'info> {
+
+    fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+        let cpi_accounts = SetAuthority {
+            account_or_mint: self.vault_account.to_account_info().clone(),
+            current_authority: self.initializer.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
 }
--- tests/gamble_match_escrow.js
+++ tests/gamble_match_escrow.js
@@ -10,24 +10,85 @@ anchor.setProvider(provider)
 
 const program = anchor.workspace.GambleMatchEscrow;
 
+
+
+//initialize token to test
+let mintA = null;
+let TokenAccountA = null
+const initializerAmount = 500;
+
+const mintAuthority = anchor.web3.Keypair.generate();
+const payer = anchor.web3.Keypair.generate();
+
+let pda = null;
+
+
 const testaccount = anchor.web3.Keypair.generate()
+
+it('test mint', async () => {
+  await provider.connection.confirmTransaction(
+    await provider.connection.requestAirdrop(payer.publicKey, 10000000000),
+    "confirmed"
+  );
+
+  //Create mintA, payer address pays, mintAuthority address gets authority
+  mintA = await Token.createMint(
+    provider.connection,
+    payer,
+    mintAuthority.publicKey,
+    null,
+    0,
+    TOKEN_PROGRAM_ID
+  );
+
+  //Create Token account A for providers wallet
+  TokenAccountA = await mintA.createAccount(provider.wallet.publicKey);
+
+  await mintA.mintTo(
+    TokenAccountA,
+    mintAuthority.publicKey,
+    [mintAuthority],
+    initializerAmount
+  );
+
+  let _TokenAccountA = await mintA.getAccountInfo(TokenAccountA);
+  
+  assert.ok(_TokenAccountA.amount.toNumber() == initializerAmount);
+});
+
+
 
 describe('gamble_match_escrow', () => {
 
   // Configure the client to use the local cluster.
-  
 
   it('Is initialized!', async () => {
     // Add your test here.
+    //create Token account for mint A
+
+    const [_pda, _nonce] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from(anchor.utils.bytes.utf8.encode("gamble_match_escrow"))],
+      program.programId
+    );
+    
+    TokenAccountPDA = await mintA.createAccount(_pda);
+    let _TokenAccountPDA = await mintA.getAccountInfo(TokenAccountPDA);
+
+    
+
     const tx = await program.rpc.initializeEscrow( {
       accounts:{
+        initializer: provider.wallet.publicKey,
+        mint: mintA.publicKey,
+        initializerDepositTokenAccount: TokenAccountA.publicKey,
         escrowAccount: testaccount.publicKey,
-        user: provider.wallet.publicKey,
-        systemProgram: SystemProgram.programId,
+        vaultAccount: TokenAccountPDA.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
       },
-      signers: [testaccount],
+      signers: [testaccount, provider.wallet.publicKey],
     });
-
 
     let escrow_account = await program.account.matchAccount.fetch(testaccount.publicKey);
     
