748dccc2a71fbb1888a09f99e2dd8206b39c1cc8
MichaelGerg <michael@DESKTOP-G0G3AVA.localdomain>
2021-10-03 15:50:23 -0700

    updates to add user

--- programs/gamble_match_escrow/src/lib.rs
+++ programs/gamble_match_escrow/src/lib.rs
@@ -2,27 +2,27 @@ use anchor_lang::prelude::*;
 use anchor_lang::prelude::borsh::BorshDeserialize;
 use anchor_spl::token::{self, SetAuthority, TokenAccount, Transfer, Mint};
 use spl_token::instruction::AuthorityType;
-
+ 
 declare_id!("AVjAc7YszkPzzyNCJ9rNM1Vh9Ve2HpWJUbi6mztCZi6D");
-
+ 
 #[program]
 pub mod gamble_match_escrow {
     use super::*;
-
+ 
     const VAULT_AUTHORITY_SEED: &[u8] = b"authority-seed";
-
+ 
     pub fn initialize_escrow(ctx: Context<InitializeEscrow>, initializer_amount: u64) -> ProgramResult {
         ctx.accounts.match_account.load();
-
+ 
         let (vault_authority, _vault_authority_bump) =
             Pubkey::find_program_address(&[VAULT_AUTHORITY_SEED], ctx.program_id);
-
+ 
         token::set_authority(
             ctx.accounts.into_set_authority_context(),
             AuthorityType::AccountOwner,
             Some(vault_authority),
         )?;
-
+ 
         token::transfer(
             ctx.accounts
                 .into_transfer_to_pda_context(),
@@ -31,37 +31,39 @@ pub mod gamble_match_escrow {
         ctx.accounts.match_account.add_user_to_match(ctx.accounts.user_account.key(), initializer_amount);
         Ok(())
     }
+ 
     pub fn add_user(ctx: Context<AddUser>, amount: u64) -> ProgramResult {
-        let (_pda, _) = Pubkey::find_program_address(&[VAULT_AUTHORITY_SEED], ctx.program_id);
-        let (vault_authority, _vault_authority_bump) =
-            Pubkey::find_program_address(&[b"authority-seed"], ctx.program_id);
-        ctx.accounts.match_account.add_user_to_match(ctx.accounts.user_account.key(), amount);
-        token::set_authority(
-            ctx.accounts.into_set_authority_context(),
-            AuthorityType::AccountOwner,
-            Some(vault_authority),
-        )?;
-        token::transfer(
-            ctx.accounts
-                .into_transfer_to_pda_context(),
-            amount,
-        )?;
-        Ok(())
+        if ctx.accounts.match_account.game_state == false {
+            let (_pda, _) = Pubkey::find_program_address(&[VAULT_AUTHORITY_SEED], ctx.program_id);
+
+            ctx.accounts.match_account.add_user_to_match(ctx.accounts.user_account.key(), amount);
+
+            token::transfer(
+                ctx.accounts
+                    .into_transfer_to_pda_context(),
+                amount,
+            )?;
+            Ok(())
+        }
+        else{
+            msg!("The game has reached maximum amount of users and is starting, please create a new lobby.");
+            Ok(())
+        }
     }
 }
-
+ 
 #[derive(Accounts)]
 pub struct InitializeEscrow<'info> {
     #[account(signer)]
     pub user_account: AccountInfo<'info>,
     pub mint: Account<'info, Mint>,
-
+ 
     #[account(mut)]
     pub initializer_deposit_token_account: Account<'info, TokenAccount>,
-
+ 
     #[account(init, payer = user_account, space = 512)]
     pub match_account: Account<'info, MatchAccount>,
-
+ 
     #[account(
         init,
         payer = user_account,
@@ -69,50 +71,51 @@ pub struct InitializeEscrow<'info> {
         token::authority = user_account,
     )]
     pub vault_account: Account<'info, TokenAccount>,
-
+ 
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
     pub token_program: AccountInfo<'info>,
 }
-
+ 
 #[derive(Accounts)]
 pub struct AddUser<'info> {
-
+ 
     #[account(signer)]
-    pub user_account: AccountInfo<'info>, // used to be initializer
-    // pub mint: Account<'info, Mint>,
-
+    pub user_account: AccountInfo<'info>, 
+    pub mint: Account<'info, Mint>,
+ 
     #[account(mut)]
     pub deposit_token_account: Account<'info, TokenAccount>,
-
+ 
     #[account(mut)]
     pub match_account: Account<'info, MatchAccount>,
-
+ 
     #[account(mut)]
     pub vault_account: Account<'info, TokenAccount>,
     pub system_program: Program<'info, System>,
-    pub rent: Sysvar<'info, Rent>,
     pub token_program: AccountInfo<'info>,
 }
-
+ 
 #[account]
 pub struct MatchAccount {
-    pub user_as_authority: Pubkey,
     pub game_state: bool,
     pub user_balances: [u64; 8],
     pub user_keys: [Pubkey; 8]
 }
-
+ 
 impl MatchAccount {
+ 
     fn empty_key() -> Pubkey {
         Pubkey::new_from_array([0u8; 32])
     }
+ 
     pub fn load(&mut self) {
         let rnd_key = MatchAccount::empty_key();
         self.game_state = false;
         self.user_balances = [0_u64; 8];
         self.user_keys = [rnd_key; 8];
     }
+ 
     fn look_for_empty_idx(&mut self) -> Option<usize> {
         let empty_key = &MatchAccount::empty_key();
         let mut i = 0;
@@ -126,15 +129,21 @@ impl MatchAccount {
         }
         result
     }
+ 
     pub fn add_user_to_match(&mut self, user_key: Pubkey, user_bal: u64) {
         let empty_idx = self.look_for_empty_idx();
-        if let Some(empty_idx) = empty_idx {
-            self.user_keys[empty_idx] = user_key;
-            self.user_balances[empty_idx] = user_bal;
+        match empty_idx {
+            Some(empty_idx) => {
+                self.user_keys[empty_idx] = user_key;
+                self.user_balances[empty_idx] = user_bal;
+            },
+            None => {
+                self.game_state = true;
+            }
         }
     }
 }
-//
+ 
 impl<'info> InitializeEscrow<'info> {
     fn into_transfer_to_pda_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
         let cpi_accounts = Transfer {
@@ -147,11 +156,11 @@ impl<'info> InitializeEscrow<'info> {
         };
         CpiContext::new(self.token_program.clone(), cpi_accounts)
     }
-
+ 
     fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
         let cpi_accounts = SetAuthority {
             account_or_mint: self
-                .initializer_deposit_token_account
+                .vault_account
                 .to_account_info()
                 .clone(),
             current_authority: self.user_account.clone(),
@@ -159,38 +168,7 @@ impl<'info> InitializeEscrow<'info> {
         CpiContext::new(self.token_program.clone(), cpi_accounts)
     }
 }
-
-// impl<'info> From<&mut InitializeEscrow<'info>>
-//     for CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
-//     fn from(accounts: &mut InitializeEscrow<'info>) -> Self {
-//         let cpi_accounts = Transfer {
-//             from: accounts
-//                 .initializer_deposit_token_account
-//                 .to_account_info()
-//                 .clone(),
-//             to: accounts.vault_account.to_account_info().clone(),
-//             authority: accounts.user_as_authority.clone(),
-//         };
-//         CpiContext::new(accounts.token_program.clone(), cpi_accounts)
-//     }
-// }
-//
-// impl<'info> From<&mut InitializeEscrow<'info>>
-//     for CpiContext<'_, '_, '_, 'info, SetAuthority<'info>>
-// {
-//     fn from(accounts: &mut InitializeEscrow<'info>) -> Self {
-//         let cpi_accounts = SetAuthority {
-//             account_or_mint: accounts
-//                 .initializer_deposit_token_account
-//                 .to_account_info()
-//                 .clone(),
-//             current_authority: accounts.initializer.clone(),
-//         };
-//         let cpi_program = accounts.token_program.clone();
-//         CpiContext::new(cpi_program, cpi_accounts)
-//     }
-// }
-
+ 
 impl<'info> AddUser<'info> {
     fn into_transfer_to_pda_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
         let cpi_accounts = Transfer {
@@ -200,13 +178,6 @@ impl<'info> AddUser<'info> {
                 .clone(),
             to: self.vault_account.to_account_info().clone(),
             authority: self.user_account.clone(),
-        };
-        CpiContext::new(self.token_program.clone(), cpi_accounts)
-    }
-    fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
-        let cpi_accounts = SetAuthority {
-            account_or_mint: self.vault_account.to_account_info().clone(),
-            current_authority: self.user_account.clone(),
         };
         CpiContext::new(self.token_program.clone(), cpi_accounts)
     }
--- tests/gamble_match_escrow.js
+++ tests/gamble_match_escrow.js
@@ -2,7 +2,7 @@ const anchor = require("@project-serum/anchor");
 const { TOKEN_PROGRAM_ID, Token } = require("@solana/spl-token");
 const assert = require("assert");
 const { Console } = require("console");
-const process = require("process");
+
 
 
 
@@ -16,16 +16,24 @@ const program = anchor.workspace.GambleMatchEscrow;
 
 //initialize token to test
 let mintA = null;
-let TokenAccountA = null
+
+let TokenAccountA = null;
+let TokenAccountB = null;
+
 const initializerAmount = 500;
 
 const mintAuthority = anchor.web3.Keypair.generate();
+
 const payer = anchor.web3.Keypair.generate();
+
+const secondUser = anchor.web3.Keypair.generate();
 
 let pda = null;
 
+//test account is being used to create a pubkey for the match account data
 const testaccount = anchor.web3.Keypair.generate()
 console.log(`\n\nANCHOR WALLET: ${process.env.ANCHOR_WALLET}`);
+
 it('test mint', async () => {
   await provider.connection.confirmTransaction(
     await provider.connection.requestAirdrop(payer.publicKey, 10000000000),
@@ -44,9 +52,17 @@ it('test mint', async () => {
 
   //Create Token account A for providers wallet
   TokenAccountA = await mintA.createAccount(provider.wallet.publicKey);
+  TokenAccountB = await mintA.createAccount(secondUser.publicKey);
 
   await mintA.mintTo(
     TokenAccountA,
+    mintAuthority.publicKey,
+    [mintAuthority],
+    initializerAmount
+  );
+
+  await mintA.mintTo(
+    TokenAccountB,
     mintAuthority.publicKey,
     [mintAuthority],
     initializerAmount
@@ -58,72 +74,66 @@ it('test mint', async () => {
 });
 
 
-describe('Initialize PDA token account', () => {
+it('Initialized!', async () => {
 
-  it('Initialized!', async () => {
+  /*
+  const [_pda, _nonce] = await anchor.web3.PublicKey.findProgramAddress(
+     [Buffer.from(anchor.utils.bytes.utf8.encode("authority-seed"))],
+     program.programId
+  );
+  */
 
-    // const [_pda, _nonce] = await anchor.web3.PublicKey.findProgramAddress(
-    //   [Buffer.from(anchor.utils.bytes.utf8.encode("authority-seed"))],
-    //   program.programId
-    // );
-
-    const tx = await program.rpc.initializeEscrow(new anchor.BN(123),{
-      accounts:{
-        userAsAuthority: provider.wallet.publicKey,
-        mint: mintA.publicKey,
-        initializerDepositTokenAccount: TokenAccountA,
-        escrowAccount: testaccount.publicKey, 
-        vaultAccount: provider.wallet.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-        tokenProgram: TOKEN_PROGRAM_ID,
-        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-      },
-      signers: [testaccount, provider.wallet.payer],
-    });
-    console.log({ testaccount: testaccount.publicKey.toString(), provider_key: provider.wallet.payer.publicKey.toString() })
-
-    let _TokenAccountPDA = await mintA.getAccountInfo(provider.wallet.publicKey);
-    let _TokenAccountA = await mintA.getAccountInfo(TokenAccountA);
-
-
-    assert.ok(_TokenAccountPDA.amount.toNumber() + _TokenAccountA.amount.toNumber() == initializerAmount);
-    /*
-    let escrow_account = await program.account.matchAccount.fetch(testaccount.publicKey);
-    assert.ok(escrow_account);
-    */
-    console.log("Your transaction signature", tx);
+  const tx = await program.rpc.initializeEscrow(new anchor.BN(123),{
+    accounts:{
+      userAccount: provider.wallet.publicKey,
+      mint: mintA.publicKey,
+      initializerDepositTokenAccount: TokenAccountA,
+      matchAccount: testaccount.publicKey, 
+      vaultAccount: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+    },
+    signers: [testaccount, provider.wallet.payer],
   });
 
-  it('Added user!', async () => {
+  let _TokenAccountPDA = await mintA.getAccountInfo(provider.wallet.publicKey);
+  let _TokenAccountA = await mintA.getAccountInfo(TokenAccountA);
 
-    const [_pda, _nonce] = await anchor.web3.PublicKey.findProgramAddress(
-        [Buffer.from(anchor.utils.bytes.utf8.encode("authority-seed"))],
-        program.programId
-    );
-    // console.log({_pda, _nonce}); // not really sure what i'm doing here but it's 5am and i'm tired yolo
-    const tx = await program.rpc.addUser(new anchor.BN(123),{
-      accounts:{
-        userAsAuthority: provider.wallet.publicKey,
-        // mint: mintA.publicKey,
-        depositTokenAccount: TokenAccountA,
-        escrowAccount: testaccount.publicKey,
-        vaultAccount: provider.wallet.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-        tokenProgram: TOKEN_PROGRAM_ID,
-        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-      },
-      signers: [provider.wallet.payer],
-    });
+  assert.ok(_TokenAccountPDA.amount.toNumber() + _TokenAccountA.amount.toNumber() == initializerAmount);
+  console.log("Your transaction signature", tx);
 
-    // let _TokenAccountPDA = await mintA.getAccountInfo(provider.wallet.publicKey);
-    // let _TokenAccountA = await mintA.getAccountInfo(TokenAccountA);
-    //
-    //
-    // assert.ok(_TokenAccountPDA.amount.toNumber() + _TokenAccountA.amount.toNumber() == initializerAmount);
-    /*
-    let escrow_account = await program.account.matchAccount.fetch(testaccount.publicKey);
-    assert.ok(escrow_account);
-    */
-    console.log("Your transaction signature", tx);
+
+  let _TokenAccountB = await mintA.getAccountInfo(TokenAccountB);
+  console.log(_TokenAccountB);
+  console.log(secondUser);
+
+  //make sure Mint matches mint
+  const tx2 = await program.rpc.addUser(new anchor.BN(123),{
+    accounts:{
+      userAccount: secondUser.publicKey,
+      mint: mintA.publicKey,
+      depositTokenAccount: TokenAccountB,
+      matchAccount: testaccount.publicKey,
+      vaultAccount: _TokenAccountPDA.address,
+      systemProgram: anchor.web3.SystemProgram.programId,
+      tokenProgram: TOKEN_PROGRAM_ID,
+    },
+    signers: [secondUser],
   });
+
+  assert.ok(true);
+  console.log("Your transaction signature", tx2);
+  
 });
+
+  
+
+  // let _TokenAccountPDA = await mintA.getAccountInfo(provider.wallet.publicKey);
+  // let _TokenAccountA = await mintA.getAccountInfo(TokenAccountA);
+  //
+  //
+  // assert.ok(_TokenAccountPDA.amount.toNumber() + _TokenAccountA.amount.toNumber() == initializerAmount);
+  // let escrow_account = await program.account.matchAccount.fetch(testaccount.publicKey);
+  // assert.ok(escrow_account);
+
