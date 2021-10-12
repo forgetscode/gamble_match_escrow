8c84824cffda690fa5429731756d2a0b42a2f6fd
bigbizze <hpcngmoh@gmail.com>
2021-10-12 00:50:58 -0700

    yolo

---
+++ test-react/app/src/App.tsx
@@ -0,0 +1,228 @@
+import './App.css';
+import { useState } from 'react';
+import { clusterApiUrl, ConfirmOptions, Connection, PublicKey } from '@solana/web3.js';
+import { Program, Provider } from '@project-serum/anchor';
+import IDL_JSON from './idl.json';
+import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
+import { ConnectionProvider, useWallet, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { Idl } from "@project-serum/anchor/src/idl";
+
+import { TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
+import * as anchor from '@project-serum/anchor';
+
+type IDL = {
+  metadata: {
+    address: string
+  }
+} & Idl;
+const idl = IDL_JSON as IDL;
+
+const wallets = [ getPhantomWallet() ]
+
+
+const opts: ConfirmOptions = {
+  preflightCommitment: "processed"
+}
+const programID = new PublicKey(idl.metadata.address);
+
+
+const escrow_account = anchor.web3.Keypair.generate()
+
+const vault_handler = anchor.web3.Keypair.generate()
+
+const deposit_amount = new anchor.BN(10000000000);
+
+function App() {
+  const [value] = useState('');
+
+  const wallet = useWallet()
+
+  async function getProvider() {
+    /* create the provider and return it to the caller */
+    /* network set to local network for now */
+    const network = clusterApiUrl('devnet');
+    const connection = new Connection(network, opts.preflightCommitment);
+
+    return new Provider(
+        connection, wallet as any,
+        opts
+    );
+  }
+
+
+
+  //mintA = "BDaZrrPYF5ns5xdYTdJ8hjTsLRQouT5P1Fh9k5SJbe76";
+  //user_token_account = "Bk5xX3fi1bdwXCrrB8c8EXy4ZQMLdCj3m2xBUvdzYzQW";
+
+
+  async function set_up() {
+
+    const provider = await getProvider();
+    /* create the program interface combining the idl, program ID, and provider */
+    const program = new Program(idl, programID, provider);
+
+
+    /*
+    try{
+      await provider.connection.confirmTransaction(
+        await provider.connection.requestAirdrop(token_minter.publicKey, 10000000000),
+        'confirmed airdrop'
+      );
+    }
+    catch(err){
+      console.log("failure to airdrop to token minter", err);
+    }
+
+
+    mintA = await Token.createMint(
+      provider.connection,
+      token_minter,
+      token_minter.publicKey,
+      null,
+      0,
+      TOKEN_PROGRAM_ID
+    );
+
+    user_token_account = await mintA.createAccount(provider.wallet.publicKey);
+
+    await mintA.mintTo(
+      user_token_account,
+      token_minter.publicKey,
+      [token_minter],
+      initializerAmount
+    );
+      */
+  }
+  async function getMint(
+      provider: Provider,
+      mintKey: string | PublicKey,
+
+  ) {
+    const _mintKey = typeof mintKey === "string" ? new PublicKey(mintKey) : mintKey;
+    const mint = new Token(
+        provider.connection,
+        _mintKey,
+        TOKEN_PROGRAM_ID,
+        provider as any
+    );
+    const userTokenAccount = await mint.getAccountInfo(provider.wallet.publicKey);
+    return [
+        mint,
+        userTokenAccount
+    ];
+  }
+
+  async function initialize() {
+
+    const provider = await getProvider();
+    /* create the program interface combining the idl, program ID, and provider */
+    const program = new Program(idl, programID, provider);
+
+    let mintA = new PublicKey("BDaZrrPYF5ns5xdYTdJ8hjTsLRQouT5P1Fh9k5SJbe76");
+    let user_token_account =new PublicKey("Bk5xX3fi1bdwXCrrB8c8EXy4ZQMLdCj3m2xBUvdzYzQW");
+
+    const [_pda, _nonce] = await anchor.web3.PublicKey.findProgramAddress(
+        [Buffer.from(anchor.utils.bytes.utf8.encode("authority-seed"))],
+        program.programId
+    );
+    let pda = _pda;
+
+    console.log(pda.toBase58());
+
+    /* interact with the program via rpc */
+    const tx = await program.rpc.initialize(deposit_amount ,{
+      accounts:{
+        initializer: provider.wallet.publicKey,
+        mint: mintA,
+        initializerDepositTokenAccount: user_token_account,
+        tempTokenAccount: vault_handler.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+      signers: [vault_handler],
+    });
+
+
+    console.log('initalize tx:', tx);
+
+    try{
+      console.log(provider.wallet.publicKey.toBase58());
+    }
+    catch (err){
+      console.log("error!", err);
+    }
+
+  }
+
+  async function transferToken() {
+    const provider = await getProvider();
+    /* create the program interface combining the idl, program ID, and provider */
+    const program = new Program(idl, programID, provider);
+
+    let mintA = new PublicKey("BDaZrrPYF5ns5xdYTdJ8hjTsLRQouT5P1Fh9k5SJbe76");
+    let send_token_account = new PublicKey("GvoAdo9C7ii3m4ohjoPgtLG1BSnRyyr3GN2YZFZTdgQo");
+    let reciever_token_account =new PublicKey("2z93tN6axmqi61peuaJEXy17fseHZfSayw67CsnLGCYy");
+
+    const [_pda, _nonce] = await anchor.web3.PublicKey.findProgramAddress(
+        [Buffer.from(anchor.utils.bytes.utf8.encode("authority-seed"))],
+        program.programId
+    );
+
+    let pda = _pda
+
+    const tx2 = await program.rpc.transferToken(deposit_amount, _nonce, {
+      accounts:{
+        mint: mintA,
+        initializerTempTokenAccount: send_token_account,
+        recieverTokenAccount: reciever_token_account,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        programSigner: pda,
+      },
+    });
+
+    console.log('initalize tx2:', tx2);
+
+  }
+
+  if (!wallet.connected) {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop:'100px' }}>
+          <WalletMultiButton />
+        </div>
+    )
+  } else {
+    return (
+        <div className="App">
+          <div>
+            {
+              !value && (<button onClick={set_up}>Set Up</button>)
+            }
+          </div>
+          <div>
+            {
+              !value && (<button onClick={initialize}>Initialize</button>)
+            }
+          </div>
+          <div>
+            {
+              !value && (<button onClick={transferToken}>Transfer Token</button>)
+            }
+          </div>
+        </div>
+    );
+  }
+}
+
+const AppWithProvider = () => (
+    <ConnectionProvider endpoint={clusterApiUrl('devnet')}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <App />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+)
+
+export default AppWithProvider;
---
+++ test-react/tests/mysolanaapp.js
@@ -0,0 +1,121 @@
+const anchor = require('@project-serum/anchor');
+const { TOKEN_PROGRAM_ID, Token } = require("@solana/spl-token");
+const assert = require("assert");
+const { Console } = require("console");
+
+
+const initializerAmount = 500;
+
+
+describe("Mysolanaapp", () => {
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Mysolanaapp;
+  it("It initializes the account", async () => {
+
+  const escrow_account = anchor.web3.Keypair.generate()
+  //initialize token minter to fund users with Token A
+  const token_minter = anchor.web3.Keypair.generate()
+  //initialize vault handler
+  const vault_handler = anchor.web3.Keypair.generate()
+  //find pda
+  const [_pda, _nonce] = await anchor.web3.PublicKey.findProgramAddress(
+    [Buffer.from(anchor.utils.bytes.utf8.encode("authority-seed"))],
+    program.programId
+  );
+  let pda = _pda;
+
+  //set initial deposit amount for the token transfers
+  const deposit_amount = new anchor.BN(123);
+
+
+  //airdrop lamports to token minter
+  await provider.connection.confirmTransaction(
+    await provider.connection.requestAirdrop(token_minter.publicKey, 10000000000),
+    'confirmed airdrop'
+  );
+
+  //check token minters balance
+  console.log("token minters balance lamports");
+  const info_third = await provider.connection.getBalance(token_minter.publicKey);
+  console.log(info_third);
+
+
+  //token minter creates token mint A
+  mintA = await Token.createMint(
+    provider.connection,
+    token_minter,
+    token_minter.publicKey,
+    null,
+    0,
+    TOKEN_PROGRAM_ID
+  );
+
+
+  //create token account for mintA for user
+  user_token_account = await mintA.createAccount(provider.wallet.publicKey);
+
+  //mint tokens 500 to first users token account
+  await mintA.mintTo(
+    user_token_account,
+    token_minter.publicKey,
+    [token_minter],
+    initializerAmount
+  );
+
+    //check token minters balance after minting A
+    console.log("token minters lamport balance after minting tokens for users");
+    const _info_fourth = await provider.connection.getBalance(token_minter.publicKey);
+    console.log(_info_fourth);
+
+
+    //initialize the main account
+    const tx = await program.rpc.initialize(deposit_amount ,{
+      accounts:{
+        initializer: provider.wallet.publicKey,
+        mint: mintA.publicKey,
+        initializerDepositTokenAccount: user_token_account,
+        escrowAccount: escrow_account.publicKey,
+        vaultHandler: vault_handler.publicKey,
+        vaultAccount: vault_handler.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+      signers: [ escrow_account,provider.wallet.payer, vault_handler],
+    });
+
+
+    console.log("vault handler account balance after initialize");
+    const _lamp = await provider.connection.getBalance(vault_handler.publicKey);
+    console.log(_lamp);
+
+    console.log("escrow balance after initialize");
+    const _lamp5 = await provider.connection.getBalance(escrow_account.publicKey);
+    console.log(_lamp5);
+
+    console.log("Vault token account owner");
+    const _info_seventh = await mintA.getAccountInfo(vault_handler.publicKey);
+    console.log(_info_seventh.owner);
+    console.log("Vault token account address");
+    console.log(_info_seventh.address);
+    console.log("vault handler address");
+    console.log(vault_handler.publicKey);
+    console.log("pda address");
+    console.log(_pda);
+
+    console.log("first user token A amount after transfer");
+    const _info_eigth = await mintA.getAccountInfo(user_token_account);
+    console.log(_info_eigth.amount.toNumber());
+
+    console.log("Vault token amount after transfer");
+    const _info_ninth = await mintA.getAccountInfo(vault_handler.publicKey);
+    console.log(_info_ninth.amount.toNumber());
+
+    console.log("match state after initialization");
+    const _info_tenth = await program.account.matchAccount.fetch(escrow_account.publicKey);
+    console.log(_info_tenth);
+
+
+  });
+});
